27c677b819efff929c989d0a272ff078c1113024
muhazizal <muhazizal.26@gmail.com>
2021-04-01 16:26:53 +0700

    change from beranda to home

--- components/Base/BaseBottomNavbar.vue
+++ components/Base/BaseBottomNavbar.vue
@@ -20,8 +20,8 @@ export default {
   data: () => ({
     links: [
       {
-        name: 'Beranda',
-        link: '/beranda',
+        name: 'Home',
+        link: '/home',
         icon: 'mdi-home',
       },
       {
