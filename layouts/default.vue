439185e49116187cf7596a01926030a150b09da1
muhazizal <muhazizal.26@gmail.com>
2021-04-01 12:28:27 +0700

    add font family

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,3 +15,9 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,17 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
