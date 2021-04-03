c3fe737fd0f1c36ad13346fa8d88a069ba55c5fb
maulanairfanf <maulanairfanf@gmail.com>
2021-04-03 10:14:44 +0700

    ad component searh to page cart

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <search></search>
     <h1>Cart Page</h1>
   </div>
 </template>
@@ -7,6 +8,9 @@
 <script>
 export default {
   name: 'CartPage',
+  components: {
+    Search: () => import('@/components/Base/Search'),
+  },
   head() {
     return {
       title: 'Fish-in - Cart',
