7d3a5b8c7af65a64fa82b83c895f2d08722936ad
maulanairfanf <maulanairfanf@gmail.com>
2021-04-03 11:30:24 +0700

    complete page cart

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,7 +1,49 @@
 <template>
   <div>
     <search></search>
-    <h1>Cart Page</h1>
+    <v-card class="pa-5 mb-5" v-for="item in items" :key="item.id">
+      <div class="d-flex align-center justify-space-between mb-2">
+        <div>
+          <span class="mt-2">{{ item.name }}</span>
+        </div>
+        <div
+          class="rounded px-2 py-2"
+          :class="{
+            'blue--text text--lighten-1 light-blue lighten-5 ':
+              'Ikan Air Laut' == item.jenisIkan,
+            'orange--text text--lighten-1 orange lighten-5':
+              'Ikan Air Tawar' == item.jenisIkan,
+          }"
+        >
+          <span>{{ item.jenisIkan }}</span>
+        </div>
+      </div>
+      <div class="d-flex align-center justify-space-between mb-2">
+        <div>
+          <small class="mt-2 text-grey font-weight-light"> Jumlah Ikan </small>
+        </div>
+        <div>
+          <span>{{ item.jumlahIkan }}kg x Rp {{ item.hargaIkan }}</span>
+        </div>
+      </div>
+      <div class="d-flex align-center justify-space-between mb-2">
+        <div>
+          <small class="mt-2 text-grey font-weight-light">Ongkir</small>
+        </div>
+        <div>
+          <span>{{ item.ongkir }}</span>
+        </div>
+      </div>
+      <v-divider></v-divider>
+      <div class="d-flex align-center justify-space-between mt-2">
+        <div>
+          <span class="mt-2">Total Harga</span>
+        </div>
+        <div>
+          <span>Rp {{ item.jumlahIkan * item.hargaIkan + item.ongkir }}</span>
+        </div>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -11,6 +53,44 @@ export default {
   components: {
     Search: () => import('@/components/Base/Search'),
   },
+
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        name: 'Ikan Bawal',
+        jenisIkan: 'Ikan Air Tawar',
+        jumlahIkan: 5,
+        hargaIkan: 50000,
+        ongkir: 15000,
+      },
+      {
+        id: 2,
+        name: 'Ikan Lele',
+        jenisIkan: 'Ikan Air Tawar',
+        jumlahIkan: 2,
+        hargaIkan: 20000,
+        ongkir: 15000,
+      },
+      {
+        id: 3,
+        name: 'Ikan Mujair',
+        jenisIkan: 'Ikan Air Laut',
+        jumlahIkan: 4,
+        hargaIkan: 30000,
+        ongkir: 15000,
+      },
+      {
+        id: 4,
+        name: 'Kepiting',
+        jenisIkan: 'Ikan Air Laut',
+        jumlahIkan: 10,
+        hargaIkan: 10000,
+        ongkir: 15000,
+      },
+    ],
+  }),
+
   head() {
     return {
       title: 'Fish-in - Cart',
