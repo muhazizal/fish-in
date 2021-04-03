6645e56d9671005607d8d27299e4fd5a61c8dce6
muhazizal <muhazizal.26@gmail.com>
2021-04-03 15:30:39 +0700

    add user list and item

---
+++ components/Base/User/UserListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-list-item :key="message.name" class="px-0">
+      <v-list-item-avatar
+        width="100%"
+        height="100%"
+        max-width="48px"
+        max-height="48px"
+      >
+        <v-img :src="message.img" />
+      </v-list-item-avatar>
+      <v-list-item-content class="align-start">
+        <v-list-item-title class="mb-0">
+          <span class="font-weight-medium">{{ message.sender }}</span>
+          <span class="ml-2 green--text">{{ message.date }}</span>
+        </v-list-item-title>
+        <v-list-item-subtitle>{{ message.content }}</v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-btn icon>
+          <v-icon color="grey lighten-1">mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-list-item-action>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserListItem',
+  props: {
+    message: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
