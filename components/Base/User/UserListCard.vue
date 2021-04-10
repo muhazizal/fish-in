870a12277212d028648d26cc24086a6c5e4a6caa
muhazizal <muhazizal.26@gmail.com>
2021-04-10 12:13:43 +0700

    fix faq routing, init pages/about-us

---
+++ components/Base/User/UserListCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'UserListCard',
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ components/Base/User/UserListItemCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'UserListItemCard',
+}
+</script>
+<style lang="scss" scoped></style>
