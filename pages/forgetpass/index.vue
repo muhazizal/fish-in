4e650ee0a02e96b2f97cb0d2187fd2b4089e8dcb
naufalalvin21 <naufalalvinchandrasa@gmail.com>
2021-04-20 18:50:24 +0700

    ForgetPassPage

---
+++ pages/forgetpass/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForgetPassPage',
+  components: {
+    LoginForm: () => import('@/components/ForgetPass/ForgetPassForm'),
+  },
+  layout: 'auth',
+  head() {
+    return {
+      title: 'ForgetPass',
+    }
+  },
+}
+</script>
